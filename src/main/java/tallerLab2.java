1fa066e90a4b1ffc29047706ebf77f9a72784bb9
dnava <d.navarro03@ufromail.cl>
2022-09-21 09:48:10 -0300

    Commit Navarro2

--- src/main/java/tallerLab2.java
+++ src/main/java/tallerLab2.java
@@ -1,2 +1,18 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class tallerLab2 {
+    public static int cantidadDias(int nDias) {
+        return nDias;
+    }
+//        boolean validador=false;
+//            try {
+//                System.out.println("Ingrese cantidad de dias: \n");
+//                Scanner teclado = new Scanner(System.in);
+//                int nDias = teclado.nextInt();
+//                return nDias;
+//            }catch (Exception InputMismatchException){
+//                System.out.println("Ingrese un valor valido");
+//                cantidadDias();
+//            }
 }
